c6f9d8657697b48e14f68fb6db7e34114708cf3f
Nikola Jevtovic <nikolejevtovica@gmail.com>
2021-01-28 20:08:20 +0100

    new functions

--- pokemon.py
+++ pokemon.py
@@ -10,19 +10,28 @@ def request_data(url):
 def all_pokemon_list():
     url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898'
     data = request_data(url)
-    all_pokemon_list = []
+    all_pokemon_list = {}
     id_count = 0
     for item in data['results']:
         id_count += 1
-        temp_dict = {'id': str(id_count), 'name': item['name']}
-        all_pokemon_list.append(temp_dict)
+        all_pokemon_list[id_count] = item['name']
     return all_pokemon_list
 
-def print_all_pokemons(pokemon_list):
-    for item in pokemon_list:
-        print(f"ID: {item['id']}\tName: {item['name'].title()}")
+def print_all_pokemons():
+    pokemon_list = all_pokemon_list()
+    for key, val in pokemon_list.items():
+        print(f"ID: {key}\tName: {val.title()}")
 
+    
 class Pokemon():
+    def pokemon_atributes(pokemon_id):
+        url = "https://pokeapi.co/api/v2/pokemon/" + str(pokemon_id)
+        pokemon_atributes = []
+        data = request_data(url)
+        for key in data:
+            pokemon_atributes.append(data[key])
+        return pokemon_atributes
+
     def __init__(self, abilities, base_experience, forms, game_indices, 
                 height, held_items, id_num, is_default, location_area_encounters, moves, 
                 name, order, species, sprites, stats, types, weight):
@@ -46,37 +55,11 @@ class Pokemon():
     
     def ability_list(self):
         abilities = []
-        ability_description_url = []
-        ability_effect = []
-        ability_short_effect = []
-        hidden_ablility = []
-        ability_data = []
-
+        hidden_ability = []
         for item in self.abilities:
             abilities.append(item['ability']['name'])
-            ability_description_url.append(item['ability']['url'])
-            hidden_ablility.append(item['is_hidden'])
-        
-        for i in range(len(abilities)):
-            data = request_data(ability_description_url[i])
-            for y in range(len(data['effect_entries'])):
-                ability_data.append(data['effect_entries'][y])
-        
-        for item in ability_data:
-            if item['language']['name'] == 'en':
-                ability_effect.append(item['effect'])
-                ability_short_effect.append(item['short_effect'])
-        
-        return abilities, hidden_aility
-
-        
-
-
-
-            # print(f"\nAbility: {abilities[i]}\nEffect:\n\t{ability_description['effect_entries'][1]['effect']}")
-            # print(f"\nShort effect:\n\t{ability_description['effect_entries'][1]['short_effect']}")
-            # if hidden_ablility[i]:
-            #     print(f"Note! This ability is hidden.")
+            hidden_ability.append(item['is_hidden'])
+        return abilities, hidden_ability
     
     def pokemon_types(self):
         types = []
@@ -84,25 +67,58 @@ class Pokemon():
             types.append(item['type']['name'])
         return types
     
-    def pokemon_species(self):
-        species = []
-        print(self.species)
-
+    def pokemon_stats(self):
+        stat_dict = {}
+        for item in self.stats:
+            stat_dict[item['stat']['name']] = item['base_stat']
+        return stat_dict
+    
+    def pokemon_color(self):
+        base_url = 'https://pokeapi.co/api/v2/pokemon-color/'
+        for i in range(1,11):
+            url = base_url + str(i)
+            data = request_data(url)
+            for i in range(len(data['pokemon_species'])):
+                if data['pokemon_species'][i]['name'] == self.name:
+                    color = data['name']
+        return color
+    
+    def pokemon_gender(self):
+        base_url = 'https://pokeapi.co/api/v2/gender/'
+        for i in range(1,4):
+            url = base_url + str(i)
+            data = request_data(url)
+            for i in range(len(data['pokemon_species_details'])):
+                if data['pokemon_species_details'][i]['pokemon_species']['name'] == self.name:
+                    gender = data['name']
+        print(gender)
 
     def pokemon_info(self):
-        print(f"Name: {self.name.title()}\nID: {self.id_num}\nHeight: {self.height/10} m\nWeight: {self.weight/10} Kg\nBase experience: {self.base_experience}")
-        
+        name = self.name
+        id_num = str(self.id_num)
+        height = str(self.height / 10) + ' m'
+        weight = str(self.weight / 10) + ' Kg'
+        base_experience = str(self.base_experience)
+        color = self.pokemon_color()
+        stats = self.pokemon_stats()
+        hp = str(stats['hp'])
+        attack = str(stats['attack'])
+        defense = str(stats['defense'])
+        sp_attack = str(stats['special-attack'])
+        sp_defense = str(stats['special-defense'])
+        speed = str(stats['speed'])
+
+
+        print(f"Name: {self.name.title()}\nID: {self.id_num}\nHeight: {self.height/10} m\nWeight: {self.weight/10} Kg\nBase experience: {self.base_experience}\nColor: {self.pokemon_color()}")
+        print(stats)
+    
 
 
 
 
+pokemon_id = 'pikachu'
+pokemon = Pokemon(*Pokemon.pokemon_atributes(pokemon_id))
+print_all_pokemons()
+print(pokemon.species)
 
-url = "https://pokeapi.co/api/v2/pokemon/25/"
-pokemon_atributes = []
-data = request_data(url)
-for key in data:
-    pokemon_atributes.append(data[key])
 
-pokemon = Pokemon(*pokemon_atributes)
-
-print(pokemon.forms)
